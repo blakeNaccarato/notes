72b384a848f415bf003fff406fa3a42590bc66f7
blakeNaccarato <blake.naccarato@gmail.com>
2023-06-12 15:16:14 -0700

    Synchronize common data properly to personal vault

--- src/notes/stages/update_common.py
+++ src/notes/stages/update_common.py
@@ -7,7 +7,7 @@ from notes.models.params import PARAMS
 
 def main():
     copytree(PARAMS.paths.common, PARAMS.paths.grad, dirs_exist_ok=True)
-    copytree(PARAMS.paths.common, PARAMS.paths.local, dirs_exist_ok=True)
+    copytree(PARAMS.paths.common, PARAMS.paths.personal, dirs_exist_ok=True)
 
 
 if __name__ == "__main__":
