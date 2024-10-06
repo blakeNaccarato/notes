b7569ccfcde594221e30b4bcfc52902d6d520c97
blakeNaccarato <blake.naccarato@gmail.com>
2024-10-06 13:45:00 -0700

    Associate PDFs with literature notes

--- src/notes/__main__.py
+++ src/notes/__main__.py
@@ -2,11 +2,13 @@
 
 from hashlib import sha256
 from pathlib import Path
+from re import sub
+from textwrap import dedent
 
 from cappa import Subcommands
 from cappa.base import command, invoke
 from pandas import read_csv
-from pydantic import BaseModel
+from pydantic import BaseModel, DirectoryPath, FilePath
 
 
 def main():
@@ -14,38 +16,72 @@ def main():
     invoke(Notes)
 
 
-class QuotesCols(BaseModel):
-    """Columns of the quotes CSV file."""
+@command
+class AssociatePdfs(BaseModel):
+    """Associate literature notes with PDFs."""
 
-    id: str = "id"
-    """Hash of quote's content."""
-    content: str = "Content"
-    """Quote's content."""
-    page: str = "Page"
-    """Page the quote is on."""
+    path: DirectoryPath = Path("data/local/vaults/personal/_notes/zotero")
+    """Path to literature notes."""
 
-
-C = QuotesCols()
+    def __call__(self):
+        """Associate literature notes with PDFs."""
+        for file in self.path.glob("*.md"):
+            pdf = f'pdf: "[[_sources/zotero/{file.stem}.pdf|{file.stem}]]"'
+            contents = file.read_text(encoding="utf-8")
+            if "pdf:" in contents and pdf not in contents:
+                raise ValueError(f"Note `{file.name}` has unexpected PDF link.")
+            if pdf in contents:
+                continue
+            file.write_text(
+                encoding="utf-8",
+                data=sub(
+                    dedent(
+                        r"""
+                        ---
+                        """
+                    ),
+                    dedent(
+                        f"""
+                        {pdf}
+                        ---
+                        """
+                    ),
+                    contents,
+                ),
+            )
 
 
 @command
 class CleanQuotes(BaseModel):
     """Clean and hash quotes exported from PDFs."""
 
-    path: Path = Path(
+    path: FilePath = Path(
         "data/local/vaults/personal/_sources/zotero/allanBlueskyAheadMultiFacility2019.csv"
     )
     """Path to the CSV file containing the quotes."""
 
     def __call__(self):
         """Hash quotes exported from PDFs."""
+
+        class QuotesCols(BaseModel):
+            """Columns of the quotes CSV file."""
+
+            id: str = "id"
+            """Hash of quote's content."""
+            content: str = "Content"
+            """Quote's content."""
+            page: str = "Page"
+            """Page the quote is on."""
+
+        c = QuotesCols()
+
         (
             read_csv(self.path)
             .assign(**{
-                C.content: lambda df: df[C.content].apply(
+                c.content: lambda df: df[c.content].apply(
                     lambda v: str(v).replace("\n", " ").strip()
                 ),
-                C.id: lambda df: df[C.content].apply(
+                c.id: lambda df: df[c.content].apply(
                     lambda v: sha256(str(v).encode()).hexdigest()[:8]
                 ),
             })
@@ -58,7 +94,7 @@ class CleanQuotes(BaseModel):
 class Notes:
     """Notes operations."""
 
-    commands: Subcommands[CleanQuotes]
+    commands: Subcommands[AssociatePdfs | CleanQuotes]
 
 
 if __name__ == "__main__":
