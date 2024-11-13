426095159b76a4baa8f90a605621645cf2f8b4af
blakeNaccarato <blake.naccarato@gmail.com>
2024-11-13 13:10:32 -0800

    Sync params

--- src/notes/models/params.py
+++ src/notes/models/params.py
@@ -1,7 +1,10 @@
 """Parameters for the data pipeline."""
 
-from pydantic import BaseModel
-from pydantic.v1 import Field
+from pathlib import Path
+from subprocess import run
+
+from pydantic import BaseModel, Field
+from yaml import safe_dump
 
 from notes.models.paths import Paths
 
@@ -17,3 +20,32 @@ PARAMS = Params()
 
 PATHS = PARAMS.paths
 """All project paths."""
+
+Path("params.yaml").write_text(
+    encoding="utf-8",
+    data=safe_dump(
+        indent=2,
+        width=float("inf"),
+        data=(_params := PARAMS.model_dump())
+        | {
+            "paths": {
+                k: v.resolve().relative_to(Path.cwd()).as_posix()
+                if isinstance(v, Path)
+                else (
+                    [v.resolve().relative_to(Path.cwd()).as_posix() for v in v]
+                    if isinstance(v, list)
+                    else {
+                        k: v.resolve().relative_to(Path.cwd()).as_posix()
+                        for k, v in v.items()
+                    }
+                )
+                for k, v in _params["paths"].items()
+            }
+        },
+    ),
+)
+run(
+    check=False,
+    capture_output=True,
+    args=[*["pwsh", "-Command"], "./Invoke-Uv.ps1 pre-commit run --all-files prettier"],
+)
