f818f289a2536ad19023650bbaa540b7512ca7a5
blakeNaccarato <blake.naccarato@gmail.com>
2025-05-09 14:59:53 -0700

    Allow cancellation

--- src/notes/pomodouroboros_at_home.py
+++ src/notes/pomodouroboros_at_home.py
@@ -10,9 +10,7 @@ Pomodouroboros at home...
 import asyncio
 import json
 import subprocess
-from asyncio import TaskGroup
-from collections.abc import Callable
-from ctypes import windll
+from asyncio import CancelledError, TaskGroup
 from datetime import UTC, datetime, timedelta
 from json import loads
 from typing import Any
@@ -27,13 +25,10 @@ DATA = (
     AsyncPath("data/local" if DEBUG else "data/local/vaults/personal/_data")
     / "pomodouroboros.json"
 )
-PERIODS = 2 if DEBUG else 4  # noqa: RUF034, RUF100
+PERIODS = 2 if DEBUG else 6  # noqa: RUF034, RUF100
 HOURS = 0 if DEBUG else 1  # noqa: RUF034, RUF100
-MINUTES = 0 if DEBUG else 10  # noqa: RUF034, RUF100
+MINUTES = 0 if DEBUG else 30  # noqa: RUF034, RUF100
 SECONDS = 5 if DEBUG else 0  # noqa: RUF034, RUF100
-BREAK_HOURS = 0  # noqa: RUF034, RUF100
-BREAK_MINUTES = 0 if DEBUG else 20  # noqa: RUF034, RUF100
-BREAK_SECONDS = 1 if DEBUG else 0  # noqa: RUF034, RUF100
 
 
 async def main():  # noqa: D103
@@ -46,46 +41,62 @@ async def main():  # noqa: D103
             tg.create_task(run_pomodoro(start.astimezone(current_tz)))
 
 
-PERIOD = timedelta(hours=HOURS, minutes=MINUTES, seconds=SECONDS)
-
-
 async def run_pomodoro(start: datetime, first: bool = False):
     """Run Pomodoro."""
     await asyncio.sleep((start - get_now()).total_seconds())
+    await record_period(start, PERIOD)
     if first:
-        await asyncio.to_thread(begin_toggl_pomodoro_focus_session)
-    await asyncio.to_thread(prompt_pom_begin, start)
-    await asyncio.sleep((PERIOD - BREAK_PERIOD).total_seconds())
-    pom = await asyncio.to_thread(prompt_pom_success, start)
-    await asyncio.sleep(BREAK_PERIOD.total_seconds())
+        await asyncio.to_thread(toggle_toggl_pomodoro)
+    try:
+        await asyncio.sleep(PERIOD.total_seconds())
+    except CancelledError as exc:
+        exc.add_note("Pomodoro cancelled.")
+        await record_period(start, get_now() - start)
+        await asyncio.to_thread(toggle_toggl_pomodoro)
+        raise
+
+
+PERIOD = timedelta(hours=HOURS, minutes=MINUTES, seconds=SECONDS)
+
+
+async def record_period(start: datetime, period: timedelta):
+    """Record start time."""
     await DATA.write_text(
         encoding="utf-8",
         data=dumps({
             **loads(await DATA.read_text(encoding="utf-8")),
-            start.isoformat(timespec="seconds"): pom,
+            ser_datetime(start): ser_datetime(start + period),
         })
         + "\n",
     )
 
 
-def begin_toggl_pomodoro_focus_session():
-    """Begin focus session on Toggl Pomodoro."""
+def ser_datetime(start):
+    """Serialize datetime."""
+    return start.isoformat(timespec="seconds")
+
+
+def toggle_toggl_pomodoro():
+    """Toggle Toggl Pomodoro."""
     subprocess.run(
         capture_output=True,
         check=True,
-        args=["pwsh", "-NonInteractive", "-NoProfile", "-Command", TOGGLE_POMODORO],
+        args=[
+            "pwsh",
+            "-NonInteractive",
+            "-NoProfile",
+            "-Command",
+            TOGGLE_TOGGL_POMODORO,
+        ],
     )
 
 
-TOGGLE_POMODORO = """\
-Import-Module 'AutoItX'; \
-Invoke-AU3MouseClick -X -1240 -Y 350; \
-Invoke-AU3MouseClick -X -1240 -Y 350; \
-Invoke-AU3MouseClick -X -1320 -Y 270; \
+TOGGLE_TOGGL_POMODORO = """\
+Import-Module 'AutoItX';
+Invoke-AU3MouseClick -X -1240 -Y 350;
+Invoke-AU3MouseClick -X -1240 -Y 350;
+Invoke-AU3MouseClick -X -1320 -Y 270;
 """
-BREAK_PERIOD = timedelta(
-    hours=BREAK_HOURS, minutes=BREAK_MINUTES, seconds=BREAK_SECONDS
-)
 
 
 def dumps(obj: dict[str, Any] | None = None) -> str:
@@ -97,45 +108,6 @@ def get_now() -> datetime:
     """Get current `datetime` in UTC."""
     return datetime.now(UTC)
 
-
-def prompt_pom_begin(start: datetime):
-    """Prompt that a Pomodoro has begun."""
-    SHOW_MSG_BOX(
-        WINDOW_HANDLE,
-        BEGIN_MSG,
-        f"{NAME}: {start.isoformat(timespec='seconds')}",
-        OK_CANCEL_ABOVE_ALL,
-    )
-
-
-NAME = "Pomodouroboros at Home"
-SHOW_MSG_BOX: Callable[[int, str, str, int], int] = windll.user32.MessageBoxW
-WINDOW_HANDLE = 0
-POM_END_MSG = "Time for a break! Did you accomplish your intention?"
-MB_OK = 0x1
-MB_YESNOCANCEL = 0x3
-MB_SYSTEMMODAL = 0x1000
-BEGIN_MSG = "Beginning focus session."
-OK_CANCEL_ABOVE_ALL = MB_OK | MB_SYSTEMMODAL
-
-
-def prompt_pom_success(start: datetime) -> str:
-    """Prompt user about Pomodoro success."""
-    return POM_END_BUTTONS[
-        SHOW_MSG_BOX(
-            WINDOW_HANDLE,
-            POM_END_MSG,
-            f"{NAME}: {start.isoformat(timespec='seconds')}",
-            YES_NO_CANCEL_ABOVE_ALL,
-        )
-    ]
-
-
-YES_NO_CANCEL_ABOVE_ALL = MB_YESNOCANCEL | MB_SYSTEMMODAL
-ID_YES = 6
-ID_NO = 7
-ID_CANCEL = 2
-POM_END_BUTTONS = {ID_YES: "🍅", ID_NO: "🥫", ID_CANCEL: "❌"}
 
 if __name__ == "__main__":
     asyncio.run(main())
