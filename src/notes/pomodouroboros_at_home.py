1a31dcac1f39554fbac1296fdd4cf57e0114b20e
blakeNaccarato <blake.naccarato@gmail.com>
2025-05-09 13:13:24 -0700

    Switch to janky Toggl desktop automation

--- src/notes/pomodouroboros_at_home.py
+++ src/notes/pomodouroboros_at_home.py
@@ -9,7 +9,8 @@ Pomodouroboros at home...
 
 import asyncio
 import json
-from asyncio import Lock, TaskGroup, run
+import subprocess
+from asyncio import TaskGroup
 from collections.abc import Callable
 from ctypes import windll
 from datetime import UTC, datetime, timedelta
@@ -17,7 +18,6 @@ from json import loads
 from typing import Any
 
 from aiopath import AsyncPath
-from playwright.async_api import Locator, PlaywrightContextManager, ViewportSize
 
 from notes.times import current_tz
 
@@ -29,7 +29,7 @@ DATA = (
 )
 PERIODS = 2 if DEBUG else 4  # noqa: RUF034, RUF100
 HOURS = 0 if DEBUG else 1  # noqa: RUF034, RUF100
-MINUTES = 0 if DEBUG else 30  # noqa: RUF034, RUF100
+MINUTES = 0 if DEBUG else 10  # noqa: RUF034, RUF100
 SECONDS = 5 if DEBUG else 0  # noqa: RUF034, RUF100
 BREAK_HOURS = 0  # noqa: RUF034, RUF100
 BREAK_MINUTES = 0 if DEBUG else 20  # noqa: RUF034, RUF100
@@ -39,42 +39,25 @@ BREAK_SECONDS = 1 if DEBUG else 0  # noqa: RUF034, RUF100
 async def main():  # noqa: D103
     if not await DATA.exists():
         await DATA.write_text(encoding="utf-8", data=dumps())
-    async with PlaywrightContextManager() as pw:
-        browser = await pw.chromium.launch(
-            args=(["--disable-blink-features=AutomationControlled", "--mute-audio"]),
-            channel="chrome",
-            headless=False,
-        )
-        ctx = await browser.new_context(
-            viewport=ViewportSize(width=WIDTH, height=HEIGHT)
-        )
-        loc, lock = (await ctx.new_page()).locator("*"), Lock()
-        async with TaskGroup() as tg:
-            now = get_now()
-            for start in [now + i * PERIOD for i in range(PERIODS)]:
-                tg.create_task(run_pomodoro(loc, lock, start.astimezone(current_tz)))
-        await loc.page.close()
-        await ctx.close()
-        await browser.close()
+    async with TaskGroup() as tg:
+        now = get_now()
+        tg.create_task(run_pomodoro(now.astimezone(current_tz), first=True))
+        for start in [now + i * PERIOD for i in range(1, PERIODS)]:
+            tg.create_task(run_pomodoro(start.astimezone(current_tz)))
 
 
 PERIOD = timedelta(hours=HOURS, minutes=MINUTES, seconds=SECONDS)
-WIDTH = 728
-HEIGHT = 536
 
 
-async def run_pomodoro(loc: Locator, lock: Lock, start: datetime):
+async def run_pomodoro(start: datetime, first: bool = False):
     """Run Pomodoro."""
-    async with lock:
-        await asyncio.sleep((start - get_now()).total_seconds())
-        await loc.page.goto(TIMER)
-        await loc.get_by_role("textbox", name=":00:00").type(
-            f"{HOURS:02}:{MINUTES:02}:{SECONDS:02}"
-        )
-        await loc.get_by_role("button", name="Start").click()
-        await asyncio.sleep((PERIOD - BREAK_PERIOD).total_seconds())
-        pom = await asyncio.to_thread(prompt_pom_success, start)
-        await asyncio.sleep(BREAK_PERIOD.total_seconds())
+    await asyncio.sleep((start - get_now()).total_seconds())
+    if first:
+        await asyncio.to_thread(begin_toggl_pomodoro_focus_session)
+    await asyncio.to_thread(prompt_pom_begin, start)
+    await asyncio.sleep((PERIOD - BREAK_PERIOD).total_seconds())
+    pom = await asyncio.to_thread(prompt_pom_success, start)
+    await asyncio.sleep(BREAK_PERIOD.total_seconds())
     await DATA.write_text(
         encoding="utf-8",
         data=dumps({
@@ -85,7 +68,21 @@ async def run_pomodoro(loc: Locator, lock: Lock, start: datetime):
     )
 
 
-TIMER = "https://www.google.com/search?q=timer"
+def begin_toggl_pomodoro_focus_session():
+    """Begin focus session on Toggl Pomodoro."""
+    subprocess.run(
+        capture_output=True,
+        check=True,
+        args=["pwsh", "-NonInteractive", "-NoProfile", "-Command", TOGGLE_POMODORO],
+    )
+
+
+TOGGLE_POMODORO = """\
+Import-Module 'AutoItX'; \
+Invoke-AU3MouseClick -X -1240 -Y 350; \
+Invoke-AU3MouseClick -X -1240 -Y 350; \
+Invoke-AU3MouseClick -X -1320 -Y 270; \
+"""
 BREAK_PERIOD = timedelta(
     hours=BREAK_HOURS, minutes=BREAK_MINUTES, seconds=BREAK_SECONDS
 )
@@ -111,6 +108,17 @@ def prompt_pom_begin(start: datetime):
     )
 
 
+NAME = "Pomodouroboros at Home"
+SHOW_MSG_BOX: Callable[[int, str, str, int], int] = windll.user32.MessageBoxW
+WINDOW_HANDLE = 0
+POM_END_MSG = "Time for a break! Did you accomplish your intention?"
+MB_OK = 0x1
+MB_YESNOCANCEL = 0x3
+MB_SYSTEMMODAL = 0x1000
+BEGIN_MSG = "Beginning focus session."
+OK_CANCEL_ABOVE_ALL = MB_OK | MB_SYSTEMMODAL
+
+
 def prompt_pom_success(start: datetime) -> str:
     """Prompt user about Pomodoro success."""
     return POM_END_BUTTONS[
@@ -123,15 +131,6 @@ def prompt_pom_success(start: datetime) -> str:
     ]
 
 
-NAME = "Pomodouroboros at Home"
-SHOW_MSG_BOX: Callable[[int, str, str, int], int] = windll.user32.MessageBoxW
-WINDOW_HANDLE = 0
-BEGIN_MSG = "Take a break!"
-POM_END_MSG = f"{BEGIN_MSG} Did you accomplish your intention?"
-MB_OK = 0x1
-MB_YESNOCANCEL = 0x3
-MB_SYSTEMMODAL = 0x1000
-OK_CANCEL_ABOVE_ALL = MB_OK | MB_SYSTEMMODAL
 YES_NO_CANCEL_ABOVE_ALL = MB_YESNOCANCEL | MB_SYSTEMMODAL
 ID_YES = 6
 ID_NO = 7
@@ -139,4 +138,4 @@ ID_CANCEL = 2
 POM_END_BUTTONS = {ID_YES: "🍅", ID_NO: "🥫", ID_CANCEL: "❌"}
 
 if __name__ == "__main__":
-    run(main())
+    asyncio.run(main())
