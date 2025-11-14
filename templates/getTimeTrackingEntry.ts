1a99ebb957d34042dc1ac8679372144b52c63eef
blakeNaccarato <blake.naccarato@gmail.com>
2025-11-14 11:16:36 -0800

    Allow alphanumeric IDs

--- templates/getTimeTrackingEntry.ts
+++ templates/getTimeTrackingEntry.ts
@@ -5,50 +5,39 @@ import type { Templater } from "./types";
  * Get time tracking entry for selection or clipboard.
  */
 export default async (): Promise<void> => {
-  const tp = (
-    (app.plugins.getPlugin("templater-obsidian") as TemplaterPlugin)
-      .templater as Templater
-  ).current_functions_object;
-  // Replace Markdown-style links with link text
-  let text: string = (await tp.user.getSelOrClip()).replace(
-    /\[(?<linkText>[^\]]*)\]\([^)]*\)/g,
-    "$<linkText>",
-  );
-  // Match the time tracking entry
   const idToken = "🆔";
-  const id = String.raw`${idToken}\s*\d{4}-\d{2}-\d{2}T\d{6}[\+-]\d{4}`;
+  const idPart = String.raw`\d{4}-\d{2}-\d{2}T\d{6}[\+-]\d{4}|[\d\w]+`;
   const emptyParens = String.raw`\(\)`; // Possible in Obsidian Tasks output
-  const groups = text.match(
-    "^" + // Beginning of string
-      String.raw`(?:\s*-)?` + // Markdown-style bullet
-      String.raw`(?:\s*\[[^\]]?\])?` + // Markdown-style checkbox
-      String.raw`(?:\s*(?<logId>${id}):)?` + // Log ID
-      "(?:s*.*#)?" + // Leading tags or header hashes, including the last `#`
-      // Compose entry from consumed characters and optional task ID
-      (String.raw`\s*(?<timeTrackingEntry>` +
-        // Recursively consume characters without capturing
+  const obsidianTasksSymbols = String.raw`⏫|⏬|⏳|⛔|✅|❌|➕|🏁|📅|🔁|🔺|🔼|🔽|🛫`;
+  // Get task ID from time tracking entry
+  const { taskId } = (
+    await (
+      (app.plugins.getPlugin("templater-obsidian") as TemplaterPlugin)
+        .templater as Templater
+    ).current_functions_object.user.getSelOrClip()
+  )
+    // Match the time tracking entry
+    .match(
+      "^" + // Beginning of string
+        String.raw`(?:\s*-)?` + // Markdown-style bullet
+        String.raw`(?:\s*\[[^\]]?\])?` + // Markdown-style checkbox
+        String.raw`(?:\s*${idToken}\s*${idPart}:)?` + // Log ID
+        "(?:s*.*#)?" + // Leading tags or header hashes, including the last `#`
+        // Compose entry from consumed characters and optional task ID
         (String.raw`\s*(?:` +
-          String.raw`(?!${idToken}|${emptyParens}|⏫|⏬|⏳|⛔|✅|❌|➕|🏁|📅|🔁|🔺|🔼|🔽|🛫)` +
-          "." + // Consume one character
-          ")*") + // Repeat consumption until negative lookahead fails
-        String.raw`\s*(?<taskId>${id})?` + // Task ID
-        ")"),
-  )?.groups as {
-    timeTrackingEntry?: string;
-    logId?: string;
-    taskId?: string;
-  };
-  // Clean up the matched time tracking entry
-  const sp = " ";
-  text = (groups.timeTrackingEntry || text)
-    .trim()
-    .replace(/^\w/, (c: string) => c.toUpperCase())
-    .concat(!groups.taskId && groups.logId ? `${sp}${groups.logId}` : "");
-  // Write the result to the clipboard
-  if (text) {
-    await navigator.clipboard.writeText(text);
-    new Notice(`Copied "${text}"`);
+          // Recursively consume characters without capturing
+          (String.raw`\s*(?:` +
+            String.raw`(?!${idToken}|${emptyParens}|${obsidianTasksSymbols})` +
+            "." + // Consume one character
+            ")*") + // Repeat consumption until negative lookahead fails
+          String.raw`\s*(?:${idToken}\s*(?<taskId>${idPart}))?` + // Task ID
+          ")"),
+    )?.groups as { taskId?: string };
+  if (!taskId) {
+    new Notice("Couldn't form time tracking entry from selection or clipboard");
     return;
   }
-  new Notice("Couldn't form time tracking entry from selection or clipboard");
+  await navigator.clipboard.writeText(taskId);
+  new Notice(`Copied "${taskId}"`);
+  return;
 };
